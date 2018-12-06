97c67c66b37f06c82b64f1528136652354e820b6
ice1x <ice1x@gmail.com>
2018-12-07 02:11:45 +0500

    Add config example

---
+++ config_example.py
@@ -0,0 +1,6 @@
+TOKEN = 'your_bot_token'
+PROXY = {'http': 'socks5h://127.0.0.1:port',
+         'https': 'socks5h://127.0.0.1:port'}
+FROM_GROUP_ID = '-67890'
+TO_GROUP_ID = '-12345'
+KEYWORD = 'test'
