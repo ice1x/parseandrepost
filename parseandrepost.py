9ee72acc9b571068605c1e1b339219de5f97d953
ice1x <ice1x@gmail.com>
2018-12-07 01:43:14 +0500

    Add reading to group

--- parseandrepost.py
+++ parseandrepost.py
@@ -10,8 +10,9 @@ telebot.apihelper.proxy = config.PROXY
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
     # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+    if config.KEYWORD in message.text:
+        bot.send_message(config.TO_GROUP_ID, message.text)
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    bot.polling(none_stop=True, interval=0, timeout=20)
