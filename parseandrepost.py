de6ea319b945c3460fe29c105e6e23c765cb22a5
ice1x <ice1x@gmail.com>
2018-12-06 23:09:04 +0500

    Add echo-bot

---
+++ parseandrepost.py
@@ -0,0 +1,17 @@
+# -*- utf-8 -*-
+import telebot
+import config
+
+
+bot = telebot.TeleBot(config.TOKEN)
+telebot.apihelper.proxy = config.PROXY
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
